3c163bea2c75e5ddea60ed623b12af9a17fe94aa
Arnas Jelizarovas <jelizarovas@gmail.com>
2024-03-03 12:48:42 -0800

    added rest of the games

--- src/App.jsx
+++ src/App.jsx
@@ -69,77 +69,77 @@ const games = [
   {
     name: "Show Me the Cookies",
     thumbnail: "/kids-games/images/PBS_Game_ShowMeTheCookies_Small.png",
-    url: "",
+    url: "https://springroll-tc.pbskids.org/cookie-monster-game-show/46b13dea32ee1c32b55da7c434124efbbb456364/release/index.html",
   },
   {
     name: "Monster Music",
     thumbnail: "/kids-games/images/PBS_Game_MonsterMusic_Small.png",
-    url: "",
+    url: "https://springroll-tc.pbskids.org/monster-music/41962a5fa87b63fd25294fc2c54b61f5f67bfca4/release/index.html",
   },
   {
     name: "Ready, Set, Grow",
     thumbnail: "/kids-games/images/PBS_Game_ReadySetGrow_Small.png",
-    url: "",
+    url: "https://springroll-tc.pbskids.org/ready-set-grow/1ee121d692b2fd4735506dd2f3fc5028141069be/release/index.html",
   },
   {
     name: "Elmo's World Games",
     thumbnail: "/kids-games/images/PBS_Game_ElmosWorldGames_WINTER_Small.png",
-    url: "",
+    url: "https://springroll-tc.pbskids.org/org.pbskids.sesame.elmosworld/fd9bf35c640f19d7d8dd5fe2ee2a9a30124549b9/release/index.html",
   },
   {
     name: "Super Salad Diner",
     thumbnail: "/kids-games/images/PBS_Game_SuperSaladDiner_Small_170915_102501.png",
-    url: "",
+    url: "https://springroll-tc.pbskids.org/sesame-street-super-salad-diner/cf4d70fb36ef09bad9949693897463cb5714b840/release/index.html",
   },
   {
     name: "Storybook Builder",
     thumbnail: "/kids-games/images/PBS_Game_StoryBookBuilder_Small_170915_102408.png",
-    url: "",
+    url: "https://springroll-tc.pbskids.org/storybook-builder/66972f6d31fb828391215cbbb1259a1420e4547f/release/index.html",
   },
   {
     name: "Rhyme Time",
     thumbnail: "/kids-games/images/PBS_Game_RhymeTime_Small.png",
-    url: "",
+    url: "https://springroll-tc.pbskids.org/grovers-rhyme-time/77fb547ee9436c66371be2f82242237dfa3614f4/release/index.html",
   },
   {
     name: "Ernie's Dinosaur Daycare",
     thumbnail: "/kids-games/images/PBS_Game_DinosaurDaycare_Small.png",
-    url: "",
+    url: "https://springroll-tc.pbskids.org/dinosaur-day-care/d35485eee6600f403076cf3263d466f24f795061/release/index.html",
   },
   {
     name: "Dress Up Time Holiday",
     thumbnail: "/kids-games/images/PBS_Game_DressUpTime_Holiday_Small_181210_100348.png",
-    url: "",
+    url: "https://springroll-tc.pbskids.org/dress-up-time/1b2cc2a67cbeb59d699e5bd2b50842ad1ed95431/release/index.html",
   },
   {
     name: "Elmo's School Friends",
     thumbnail: "/kids-games/images/PBS_Game_ElmosSchoolFriends_Small_170915_102144.png",
-    url: "",
+    url: "https://springroll-tc.pbskids.org/elmos-school-friends/efca78593f34ac18847cd98192da97f3034ca8f4/release/index.html",
   },
   {
     name: "Abby's Sandbox Search",
     thumbnail: "/kids-games/images/PBS_Game_AbbysSandboxSearch_Small.png",
-    url: "",
+    url: "https://springroll-tc.pbskids.org/abbys-sandbox-search/16b1fcdb0f4041925daa43be6d9f5f9682b11bb6/release/index.html",
   },
   {
     name: "The Cookie Games",
     thumbnail: "/kids-games/images/PBS_Game_CookieGames_Small_170915_101936.png",
-    url: "",
+    url: "https://springroll-tc.pbskids.org/the-cookie-games/e5ad232b5b12e540b4bf13dfdfeeab29a5b56130/release/index.html",
   },
   {
     name: "Grover's Winter Games",
     thumbnail: "/kids-games/images/PBS_Game_GroversWinterGames_Small.png",
-    url: "",
+    url: "https://springroll-tc.pbskids.org/sesame-street-grovers-winter-games/b2d8f070d49849215ddfe3a3737997668e09505f/release/index.html",
   },
   {
     name: "Oscar's Rotten Ride",
     thumbnail: "/kids-games/images/PBS_Game_OscarsRottenRide_Small.png",
-    url: "",
+    url: "https://springroll-tc.pbskids.org/sesame-street-oscars-rotten-ride/acae218652ea3f04e5c1728a999b1e37068f2a92/release/index.html",
   },
   {
     name: "The Cookie Case",
     thumbnail: "/kids-games/images/PBS_Game_CookieCase_Small.png",
-    url: "",
+    url: "https://springroll-tc.pbskids.org/cookie-case/053cbdc285031df001b63a258be83d4f60ec229c/release/index.html",
   },
 ];
 
